00aef59af424ce8436710e5d5cfe96db9e8aae06
xfitgd <xfitgd@gmail.com>
2024-08-02 08:52:42 +0900

    index buffer 완성 및 오브젝트 개념 구현

--- main.zig
+++ main.zig
@@ -31,29 +31,50 @@ const file = @import("zig-game-engine-project/file.zig");
 const graphics = @import("zig-game-engine-project/graphics.zig");
 
 pub var v: *graphics.vertices(graphics.color_vertex_2d) = undefined;
-pub var objects: ArrayList(*graphics.ivertices) = ArrayList(*graphics.ivertices).init(allocator);
+pub var i: *graphics.indices16 = undefined;
+pub var o: *graphics.shape2d = undefined;
+pub var objects: ArrayList(*graphics.iobject) = ArrayList(*graphics.iobject).init(allocator);
 pub var mem_pool: MemoryPool(graphics.dummy_vertices) = MemoryPool(graphics.dummy_vertices).init(allocator);
+pub var mem_pool2: MemoryPool(graphics.dummy_object) = MemoryPool(graphics.dummy_object).init(allocator);
+pub var mem_pool3: MemoryPool(graphics.dummy_indices) = MemoryPool(graphics.dummy_indices).init(allocator);
 
 pub fn xfit_init() void {
     v = graphics.take_vertices(v, mem_pool.create() catch unreachable);
+    o = graphics.take_object(o, mem_pool2.create() catch unreachable);
+    i = graphics.take_indices(i, mem_pool3.create() catch unreachable);
+    o.* = graphics.shape2d.init();
     v.* = graphics.vertices(graphics.color_vertex_2d).init(allocator);
+    i.* = graphics.indices16.init(allocator);
     v.*.array.append(.{
-        .pos = .{ 0, -0.5 },
+        .pos = .{ -0.5, -0.5 },
         .color = .{ 1, 0, 0, 1 },
     }) catch unreachable;
     v.*.array.append(.{
-        .pos = .{ 0.5, 0.5 },
+        .pos = .{ 0.5, -0.5 },
         .color = .{ 0, 1, 0, 1 },
     }) catch unreachable;
     v.*.array.append(.{
         .pos = .{ -0.5, 0.5 },
         .color = .{ 0, 0, 1, 1 },
     }) catch unreachable;
-    v.*.build(.read_gpu) catch unreachable;
+    v.*.array.append(.{
+        .pos = .{ 0.5, 0.5 },
+        .color = .{ 1, 1, 1, 1 },
+    }) catch unreachable;
+    i.*.array.append(0) catch unreachable;
+    i.*.array.append(1) catch unreachable;
+    i.*.array.append(2) catch unreachable;
+    i.*.array.append(1) catch unreachable;
+    i.*.array.append(3) catch unreachable;
+    i.*.array.append(2) catch unreachable;
+    v.*.build(.read_gpu);
+    i.*.build(.read_gpu);
 
     //system.print("{d}\n", .{@sizeOf(graphics.vertices(u8))});
 
-    objects.append(&v.interface) catch unreachable;
+    o.vertices = v;
+    o.indices = i;
+    objects.append(&o.interface) catch unreachable;
     graphics.scene = &objects.items;
 }
 
@@ -61,8 +82,11 @@ pub fn xfit_update() void {}
 
 pub fn xfit_destroy() void {
     v.*.deinit();
+    i.*.deinit();
     objects.deinit();
     mem_pool.deinit();
+    mem_pool2.deinit();
+    mem_pool3.deinit();
 }
 
 pub fn xfit_activate() void {}
