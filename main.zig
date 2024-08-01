254f245c10a460e8edc316a71950b38df1725b06
xfitgd <xfitgd@gmail.com>
2024-08-02 05:11:19 +0900

    index buffer 구현중

--- main.zig
+++ main.zig
@@ -17,10 +17,12 @@ const xfit = @import("zig-game-engine-project/xfit.zig");
 const system = @import("zig-game-engine-project/system.zig");
 
 const ArrayList = std.ArrayList;
+const MemoryPool = std.heap.MemoryPool;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
 const math = @import("zig-game-engine-project/math.zig");
+const mem = @import("zig-game-engine-project/mem.zig");
 const matrix = math.matrix;
 const matrix4x4 = math.matrix4x4;
 const matrix_error = math.matrix_error;
@@ -28,24 +30,28 @@ const file = @import("zig-game-engine-project/file.zig");
 
 const graphics = @import("zig-game-engine-project/graphics.zig");
 
-pub var v: graphics.vertices(graphics.color_vertex_2d) = .{};
+pub var v: *graphics.vertices(graphics.color_vertex_2d) = undefined;
 pub var objects: ArrayList(*graphics.ivertices) = ArrayList(*graphics.ivertices).init(allocator);
+pub var mem_pool: MemoryPool(graphics.dummy_vertices) = MemoryPool(graphics.dummy_vertices).init(allocator);
 
 pub fn xfit_init() void {
-    v = graphics.vertices(graphics.color_vertex_2d).init(allocator);
-    v.array.append(.{
+    v = graphics.take_vertices(v, mem_pool.create() catch unreachable);
+    v.* = graphics.vertices(graphics.color_vertex_2d).init(allocator);
+    v.*.array.append(.{
         .pos = .{ 0, -0.5 },
         .color = .{ 1, 0, 0, 1 },
     }) catch unreachable;
-    v.array.append(.{
+    v.*.array.append(.{
         .pos = .{ 0.5, 0.5 },
         .color = .{ 0, 1, 0, 1 },
     }) catch unreachable;
-    v.array.append(.{
+    v.*.array.append(.{
         .pos = .{ -0.5, 0.5 },
         .color = .{ 0, 0, 1, 1 },
     }) catch unreachable;
-    v.build() catch unreachable;
+    v.*.build(.read_gpu) catch unreachable;
+
+    //system.print("{d}\n", .{@sizeOf(graphics.vertices(u8))});
 
     objects.append(&v.interface) catch unreachable;
     graphics.scene = &objects.items;
@@ -54,8 +60,9 @@ pub fn xfit_init() void {
 pub fn xfit_update() void {}
 
 pub fn xfit_destroy() void {
-    v.destroy();
+    v.*.deinit();
     objects.deinit();
+    mem_pool.deinit();
 }
 
 pub fn xfit_activate() void {}
