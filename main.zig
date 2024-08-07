e730b9403d3a8fe4e0fb072820257f45b54f89f3
xfitgd <xfitgd@gmail.com>
2024-08-08 03:14:24 +0900

    uniform 버퍼 및 공간 좌표계 구현->math 행렬 오류 수정

--- main.zig
+++ main.zig
@@ -34,13 +34,21 @@ pub var vertices_mem_pool: MemoryPool(graphics.dummy_vertices) = MemoryPool(grap
 pub var objects_mem_pool: MemoryPool(graphics.dummy_object) = MemoryPool(graphics.dummy_object).init(allocator);
 pub var indices_mem_pool: MemoryPool(graphics.dummy_indices) = MemoryPool(graphics.dummy_indices).init(allocator);
 
+pub var g_proj: graphics.projection = undefined;
+pub var g_camera = graphics.camera.init(.{ 0, 0, -3, 1 }, .{ 0, 0, 0, 1 }, .{ 0, 1, 0, 1 });
+
 pub fn xfit_init() void {
     const vertices = graphics.take_vertices(*graphics.vertices(graphics.color_vertex_2d), vertices_mem_pool.create() catch unreachable);
     const indices = graphics.take_indices(*graphics.indices16, indices_mem_pool.create() catch unreachable);
     const object = graphics.take_object(*graphics.shape2d, objects_mem_pool.create() catch unreachable);
-    object.* = graphics.shape2d.init();
+    g_proj = graphics.projection.init(.perspective, std.math.degreesToRadians(45)) catch unreachable;
+
+    g_camera.build(.readwrite_cpu);
+    g_proj.build(.readwrite_cpu);
+
+    object.* = graphics.shape2d.init(&g_camera, &g_proj);
     vertices.* = graphics.vertices(graphics.color_vertex_2d).init(allocator);
-    indices.* = graphics.indices16.init(allocator);
+    indices.* = graphics.indices.init(allocator);
     vertices.*.array.append(.{
         .pos = .{ -0.5, -0.5 },
         .color = .{ 1, 0, 0, 1 },
@@ -61,19 +69,42 @@ pub fn xfit_init() void {
     vertices.*.build(.read_gpu);
     indices.*.build(.read_gpu);
 
+    object.build(.readwrite_cpu);
+
     object.vertices = vertices;
     object.indices = indices;
     objects.append(&object.interface) catch unreachable;
     graphics.scene = &objects.items;
 }
 
-pub fn xfit_update() void {}
+//g_proj은 렌더 스레드 내에서 처리해야 해서 Semaphore를 사용해 창 사이즈 바뀔 시 통보 받는 식으로 처리함.
+var size_update_sem: std.Thread.Semaphore = .{};
+pub fn xfit_update() void {
+    var need_size_update = true;
+    size_update_sem.timedWait(0) catch {
+        need_size_update = false; //?need_size_update 변수를 안쓰는 방법이 있나..?
+    };
+    if (need_size_update) {
+        //system.print_debug("need size update\n", .{});
+        g_proj.init_matrix(.perspective, std.math.degreesToRadians(45)) catch unreachable;
+        g_proj.map_update();
+    }
+}
+
+pub fn xfit_size() void {
+    size_update_sem.post();
+}
 
 pub fn xfit_destroy() void {
     const ivertices = objects.items[0].*.get_ivertices(objects.items[0]);
     const iindices = objects.items[0].*.get_iindices(objects.items[0]);
     ivertices.?.*.deinit(ivertices.?);
     iindices.?.*.deinit(iindices.?);
+
+    g_camera.clean();
+    g_proj.clean();
+
+    objects.items[0].*.deinit();
     objects.deinit();
     vertices_mem_pool.deinit();
     objects_mem_pool.deinit();
